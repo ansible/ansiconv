185f3d3c03cb46e9712a8da99dde5f5f1588598a
Dan Horrigan <dan@dhorrigan.com>
2013-08-23 12:20:47 -0400

    Adding css_rule test, and making tests easier to read

--- test_ansiconv.py
+++ test_ansiconv.py
@@ -1,7 +1,19 @@
 import ansiconv 
 
+
 def test_basic_to_plain():
-    assert ansiconv.to_plain('\033[0;32mFoo') == 'Foo'
+    expected = 'Foo'
+    actual = ansiconv.to_plain('\033[0;32mFoo')
+    assert actual == expected
+
 
 def test_basic_to_html():
-    assert ansiconv.to_html('\033[0;32mFoo') == '<span class="ansi32">Foo</span>'
+    expected = '<span class="ansi32">Foo</span>'
+    actual = ansiconv.to_html('\033[0;32mFoo')
+    assert actual == expected
+
+
+def test_css_rule():
+    expected = '.foo { color: #000000; }'
+    actual = ansiconv.css_rule('.foo', color="#000000")
+    assert expected == actual
